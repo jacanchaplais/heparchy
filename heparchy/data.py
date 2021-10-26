1555b8a13730c037a281db3a21bfd4b508a2b851
Jacan Chaplais <jlc1n20@soton.ac.uk>
2021-10-26 15:32:33 +0100

    signal masker: commented; added graceful output when no signal

--- heparchy/data.py
+++ heparchy/data.py
@@ -16,7 +16,7 @@ def val_elems_int(instance, attribute, value):
 def val_int_array(instance, attribute, value):
     pass
 
-@attr.s(kw_only=True, frozen=True)
+@attr.s(kw_only=True, frozen=True, slots=True)
 class SignalVertex:
     """Data structure to define which of the vertices represent a
     signal in an event, and which of the given vertex's descendants
@@ -40,7 +40,7 @@ class SignalVertex:
     follow: __PdgSet = attr.ib(**__pdg_kwargs)
 
 @attr.s(on_setattr=attr.setters.convert)
-class EventDataset:
+class ShowerData:
     import pandas as __pd
     import networkx as __nx
 
@@ -58,6 +58,22 @@ class EventDataset:
             **__array_kwargs)
 
     def flush_cache(self):
+        """Clears cached private attributes of ShowerData instance.
+        These must currently be cleared manually after updating
+        public attributes of an instance.
+
+        Notes
+        -----
+        If looping through many events, performance is vastly improved
+        if only one instance of the ShowerData class is created,
+        and the attributes of that instance are iteratively updated.
+        In this case, it is necessary to use `flush_cache` during each
+        iteration, so the next event does not have cached data from the
+        previous one.
+
+        In a future release, this will be handled automatically whenever
+        an attribute value is updated.
+        """
         try:
             self.__shower
         except AttributeError:
@@ -66,6 +82,20 @@ class EventDataset:
             del self.__shower
 
     def to_networkx(self, data=['pdg']):
+        """Output directed acyclic graph representation of the shower,
+        implemented by NetworkX. Each edge is a particle, and each node
+        is an interaction vertex, except for the terminating leaf nodes.
+
+        Parameters
+        ----------
+        data : iterable of strings
+            Specify which of the particle properties should be embedded
+            on the edges.
+            Valid options are:
+                - pdg (default)
+                - pmu
+                - final
+        """
         # form edges with pdg data on for easier ancestry tracking
         names = data
         data_rows = (getattr(self, name) for name in names)
@@ -84,7 +114,6 @@ class EventDataset:
             except AttributeError:
                 self.__shower = self.to_networkx()
             return func(self, *args, **kwargs)
-        inner.__doc__ = func.__doc__
         return inner
 
     def to_pandas(self, data=('pdg', 'final')):
@@ -153,10 +182,6 @@ class EventDataset:
         """Returns a generator object which loops over all interaction
         vertices in the event, yielding the vertex id, and pdg codes
         of incoming and outgoing particles to the vertex, respectively.
-
-        Examples
-        --------
-
         """
         shower = self.__shower
         for vertex in shower:
@@ -169,34 +194,75 @@ class EventDataset:
     
     @__requires_shower
     def signal_mask(self, signal_vertices):
+        """Locates given vertices, based on their incoming and outgoing
+        particles.
+
+        Parameters
+        ----------
+        signal_vertices : iterable of `heparchy.SignalVertex` objects
+            Specify multiple vertices to locate, and which of their
+            descendants to follow.
+            See `heparchy.SignalVertex` for information.
+
+        Returns
+        -------
+        signal_masks : list of dicts containing 1d boolean numpy arrays
+            Masks over all particles in the event, identifying which
+            descend from the followed particle, produced at the
+            chosen vertex.
+            The output for each vertex is returned, in the same order as
+            provided, as an element in a list.
+            These elements are dictionaries, whose keys refer to the pdg
+            codes of the followed particles, and whose values are the
+            boolean arrays which identify their descendants.
+        """
+        # --- LOCATE SIGNAL VERTICES IN SHOWER --- #
         is_signal = []
+        # mark where the signal vertices are in the shower:
         for vertex, pdgs_in, pdgs_out in self.vertex_pdg():
             is_signal.append(tuple(
                 [vertex] + [sig_vtx.incoming.issubset(pdgs_in)
                             and sig_vtx.outgoing.issubset(pdgs_out)
                             for sig_vtx in signal_vertices]
                 ))
+        # split into two iterables of vertex ids and booleans, respectively
         is_signal = list(zip(*is_signal))
         vertices = is_signal[0]
-        is_signal = is_signal[1:]
-        signal_ids = [vertices[sig_list.index(True)] for sig_list in is_signal]
+        signal_lists = is_signal[1:]
+        # --- POPULATE A LIST OF DICTIONARIES WITH THE SIGNAL MASKS --- #
         signal_masks = []
-        for signal_id, signal_vertex in zip(signal_ids, signal_vertices):
-            follow_masks = dict()
-            edges_out_info = self.__shower.out_edges(signal_id, data='pdg')
-            follow_edges = filter(
-                    lambda edge: edge[-1] in signal_vertex.follow,
-                    edges_out_info
-                    )
-            _, follow_ids, follow_pdgs = zip(*follow_edges)
-            for follow_id, follow_pdg in zip(follow_ids, follow_pdgs):
-                desc_vtxs = self.__nx.descendants(self.__shower, follow_id)
-                desc_edges = list(self.__shower.edges(nbunch=desc_vtxs))
-                desc_edges = structure_edges(np.array(desc_edges))
-                mask = np.isin(self.edges, desc_edges, assume_unique=True)
-                follow_masks.update({follow_pdg: mask})
+        # iterate through id and corresponding SignalVertex object together
+        for signal_list, signal_vertex in zip(signal_lists, signal_vertices):
+            follow_masks = dict() # to hold the mask, keyed by pdg
+            if True in signal_list:
+                # find the vertex id of the signal vertices:
+                signal_id = vertices[signal_list.index(True)]
+                # identify which vertex products need to be followed:
+                edges_out_info = self.__shower.out_edges(signal_id, data='pdg')
+                follow_edges = filter(
+                        lambda edge: edge[-1] in signal_vertex.follow,
+                        edges_out_info
+                        )
+                # store incident vtx of followed pcl as follow_id
+                _, follow_ids, follow_pdgs = zip(*follow_edges)
+                # find the descendants of follow_id vtxs for each followed pcl
+                for follow_id, follow_pdg in zip(follow_ids, follow_pdgs):
+                    desc_vtxs = self.__nx.descendants(self.__shower, follow_id)
+                    # convert into structured np array for set comparison
+                    desc_edges = list(self.__shower.edges(nbunch=desc_vtxs))
+                    desc_edges = structure_edges(np.array(desc_edges))
+                    # create mask identifying edges / pcls desc from signal
+                    mask = np.isin(self.edges, desc_edges, assume_unique=True)
+                    follow_masks.update({follow_pdg: mask}) # store to dict
+            else: # if vertex not located, fill with all False masks
+                mask = np.full_like(self.final, fill_value=False)
+                follow_masks.update(
+                    {follow_pdg: mask for follow_pdg in signal_vertex.follow})
             signal_masks += [follow_masks]
         return signal_masks
 
     def copy(self):
+        """Returns a copy of the ShowerData instance as a new object
+        in memory.
+        """
         return deepcopy(self)
