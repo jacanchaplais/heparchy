d432f00c9f2d22e1962b835c5d95811b14a1435a
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-11-01 15:46:57 +0000

    export version, and only show deprecation notice once

--- heparchy/__init__.py
+++ heparchy/__init__.py
@@ -4,7 +4,11 @@
 
 The hierarchically formatted high energy physics IO library.
 """
+import warnings
+
 from heparchy import read, write
+from heparchy._version import __version__, __version_tuple__
 
 
-__all__ = ["read", "write"]
+__all__ = ["read", "write", "__version__", "__version_tuple__"]
+warnings.filterwarnings("once", category=DeprecationWarning)
